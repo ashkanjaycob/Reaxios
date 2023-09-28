22f8c2b2e8f0898ee9d456e50bea9a9932a2f326
ashkanjaycob <ashkan.yaghobii@gmail.com>
2023-09-28 09:58:53 +0330

    Initialize app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+
+
+  const [usename, setUsername] = useState("haj Ashkan")
+  const [persons, setPersons] = useState([])
+  const [ShowPers, setShowPers] = useState(false)
+
+  const apiHandler = () => {
+    // Make a request for a user with a given ID
+    return axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        // handle success
+        console.log(res.data[1]);
+        setPersons((pervState) => (pervState = res.data ));
+        console.log(persons);
+      })
+      .catch(err => {
+        // handle error
+        console.log(err);
+      });
+  }
+
+
+  const userupdateHandler = () => {
+    setUsername("Haj Kiddo")
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>{usename}</p>
+        <br></br>
+        <button onClick={apiHandler}>Show Users</button>
+        <br></br>
+        <button onClick={userupdateHandler}>Change Name</button>
+        {
+          ShowPers &&
+          <p>
+            {
+
+            }
+          </p>
+        }
       </header>
     </div>
   );
